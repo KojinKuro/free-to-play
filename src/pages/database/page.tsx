07967ac881cf6f997081fb6ac5b98b842677fcdf
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-06-04 16:20:27 -0700

    Refactor: change spelling

--- src/pages/database/page.tsx
+++ src/pages/database/page.tsx
@@ -1,8 +1,8 @@
 import { useContext, useState } from "react";
 import { GameContext } from "../../App";
 import { CategorySelect } from "../../components/CategorySelect/CategorySelect";
-import GameFilter from "../../components/Game/GameFIlter/GameFilter";
 import GameFeature from "../../components/Game/GameFeature/GameFeature";
+import GameFilter from "../../components/Game/GameFilter/GameFilter";
 import GameGrid from "../../components/Game/GameGrid/GameGrid";
 import { filterGames } from "../../utils/game";
 
